8f34648524faf85abd142c9f435400cc8e94c56e
Krunal Dudhatra <126391623+krunaldudhatra@users.noreply.github.com>
2025-03-26 02:01:33 +0530

    Update AnswerRoutes.js

--- BackEnd/Routes/AnswerRoutes.js
+++ BackEnd/Routes/AnswerRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { authenticateUser } = require("../Controllers/AuthController");
 
-const {createAnswer,getAllAnswers,getAnswerById,deleteAnswer,updateAnswer,getAnswersByUserId,getAllAnswersByUsername,toggleLikeOnAnswer} = require("../Controllers/AnswerController");
+const {createAnswer,getAllAnswers,getAnswerById,deleteAnswer,updateAnswer,getAnswersByUserId,getAllAnswersByUsername,toggleLikeOnAnswer,rateAnswer} = require("../Controllers/AnswerController");
 
 router.post("/create",authenticateUser, createAnswer);
+router.post("/rating",authenticateUser, rateAnswer);
 router.get("/", getAllAnswers);
 router.post("/togglelikeAnswer",authenticateUser,toggleLikeOnAnswer)  
 router.get("/getanswer/:answerId",authenticateUser,getAnswerById);
