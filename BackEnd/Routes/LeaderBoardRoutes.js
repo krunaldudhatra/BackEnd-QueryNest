53948e1a8f96de37a9f89df6a936efaeab4763db
Krunal Dudhatra <126391623+krunaldudhatra@users.noreply.github.com>
2025-03-26 02:02:55 +0530

    Update and rename OverallLeaderBoardRoutes.js to LeaderBoardRoutes.js

--- BackEnd/Routes/LeaderBoardRoutes.js
+++ BackEnd/Routes/LeaderBoardRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const { 
     
     getLeaderboard,
-    generateAllLeaderboards} = require("../Controllers/OverallLeaderBoardController");
+    generateAllLeaderboards,getTopUsers} = require("../Controllers/LeaderBoardController");
 
 // ✅ Route to generate & store leaderboard
 router.post("/generate", generateAllLeaderboards);
-
+router.get("/gettopusers",getTopUsers);
 router.get("/",getLeaderboard);
 
 module.exports = router;
